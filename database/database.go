7aea2d77c8c54acd5d1798a3946fe8bfe2de97f2
Luigi Salcedo <csalcedoe@unicartagena.edu.co>
2023-11-14 20:12:32 -0500

    create:database connection with principal methods

---
+++ database/database.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func init() {
+	dbData := os.Getenv("vocaportal_db")
+
+	if len(dbData) == 0 {
+		log.Fatal("database data not found")
+	}
+
+	database, err := sql.Open("postgres", dbData)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db = database
+}
+
+func DB() *sql.DB {
+	return db
+}
+
+func Begin() (*sql.Tx, error) {
+	return db.Begin()
+}
+
+func BeginTx(ctx context.Context) (*sql.Tx, error) {
+	return db.BeginTx(ctx, &sql.TxOptions{
+		Isolation: sql.LevelSerializable,
+		ReadOnly:  false,
+	})
+}
+
+func Conn(ctx context.Context) (*sql.Conn, error) {
+	return db.Conn(ctx)
+}
+
+func InitStmt(sql, repository string) *sql.Stmt {
+	stmt, err := db.Prepare(sql)
+
+	if err != nil {
+		log.Fatalf("Error in '%s' repository: %v\n", repository, err)
+	}
+
+	return stmt
+}
