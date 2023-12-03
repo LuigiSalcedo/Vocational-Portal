5a4255252f133b0c94f24c51614daf65c6f2ac42
Luigi Salcedo <csalcedoe@unicartagena.edu.co>
2023-12-02 21:21:02 -0500

    added:programmes descriptions

--- models/programms_models.go
+++ models/programms_models.go
@@ -7,6 +7,7 @@ import (
 // Academic programm database model
 type Programm struct {
 	DefaultData
+	Description string `json:"string"`
 }
 
 // PWAR = Program With Area Relation
@@ -20,21 +21,33 @@ func CreateProgramm() *Programm {
 	return &Programm{}
 }
 
+func (p *Programm) Extract() []any {
+	data := make([]any, 0, 3)
+	data = append(data, p.DefaultData.Extract()...)
+	data = append(data, p.Description)
+	return data
+}
+
+func (p *Programm) Recovery(data ...any) {
+	p.DefaultData.Recovery(data[:2]...)
+	p.Description = data[2].(string)
+}
+
 // Create a program with area relation
 func CreatePWAR() *PWAR {
 	return &PWAR{}
 }
 
 func (p *PWAR) Extract() []any {
-	data := make([]any, 0, 3)
+	data := make([]any, 0, 4)
 	data = append(data, p.Programm.Extract()...)
 	data = append(data, p.N)
 	return data
 }
 
 func (p *PWAR) Recovery(data ...any) {
-	p.Programm.Recovery(data[:2]...)
-	n, err := strconv.ParseFloat(string(data[2].([]uint8)), 64)
+	p.Programm.Recovery(data[:3]...)
+	n, err := strconv.ParseFloat(string(data[3].([]uint8)), 64)
 
 	if err != nil {
 		panic(err)
